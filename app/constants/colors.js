314cb4d01104ba340e18b8f36d49c38e9500a482
Alfredo Garcia <95046266+AldG99@users.noreply.github.com>
2025-03-28 19:13:47 -0600

    Definir paleta de colores para la aplicación

--- app/constants/colors.js
+++ app/constants/colors.js
@@ -1,9 +1,19 @@
-import React from 'react';
-
-const Colors = () => {
-  return <div>Colors</div>;
+export default {
+  primary: '#4A6FFF',
+  primaryDark: '#3352D4',
+  primaryLight: '#7B97FF',
+  secondary: '#F9A826',
+  background: '#F5F7FB',
+  white: '#FFFFFF',
+  black: '#242424',
+  text: '#333333',
+  textLight: '#666666',
+  gray: '#CCCCCC',
+  grayLight: '#E8E8E8',
+  success: '#4CAF50',
+  danger: '#F44336',
+  warning: '#FFC107',
+  info: '#2196F3',
+  border: '#DDDDDD',
+  transparent: 'transparent',
 };
-
-export default Colors;
-
-// Colores de la aplicación
